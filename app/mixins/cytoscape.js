adc87b604303b3d42a438be15d786ad1d8e6cdcc
YannBeauxis <pro@yannbeauxis.net>
2018-09-08 14:33:02 +0200

    chemdoodle integration graph drawing

--- app/mixins/cytoscape.js
+++ app/mixins/cytoscape.js
@@ -7,6 +7,7 @@ export default Mixin.create({
 
 	actions: {
 		startCytoscape(data) {
+			let _this = this;
 			let colors = {
 				primary: '#073877',
 				secondary: '#636a71',
@@ -91,32 +92,70 @@ export default Mixin.create({
 
 				this.set('cy',cy);
 
-				var makeTippy = function(node){
-					node.hasTippy = true;
-					let text = node.data('name');
-					if (node.data('nodeType') === 'molecule') {
-						text = node.data('smiles');
-					}
-					let t = tippy( node.popperRef(), {
-						html: (function(){
-							var div = document.createElement('div');
-							div.innerHTML = text;
-							return div;
-						})(),
-						trigger: 'manual',
-						arrow: true,
-						// placement: 'bottom',
-						hideOnClick: false,
-						multiple: false,
-						sticky: true,
-						stickyDuration: 0,
-					} ).tooltips[0];
-					node.tippy = t;
-					return t;
+				var makeTippy = function(node) {
+				 	return new Promise(function(resolve, reject) {
+						var _makeTippy = function(text) {
+							let t = tippy( node.popperRef(), {
+							html: (function(){
+								var div = document.createElement('div');
+								div.innerHTML = text;
+								return div;
+							})(),
+							trigger: 'manual',
+							arrow: true,
+							// placement: 'bottom',
+							theme: 'light',
+							hideOnClick: false,
+							multiple: false,
+							sticky: true,
+							stickyDuration: 0,
+							} ).tooltips[0];
+							node.tippy = t;
+							return t;
+						};
+
+						node.hasTippy = true;
+						let text = node.data('name');
+
+						if (node.data('nodeType') === 'molecule') {
+							var id = node.data('id');
+							var smiles = node.data('smiles');
+							var molFile = node.data('molFile');
+							let text =`
+								<canvas						    class="ChemDoodleWebComponent"
+							    id="${id}" width="200" height="200"
+							    alt="ChemDoodle Web Component"
+							    style="width: 100px; height: 100px; background-color: rgb(255, 255, 255);">
+							      This browser does not support HTML5/Canvas.
+							  </canvas>
+
+							`;
+							//								<p>${smiles}</p>
+							let tippy = _makeTippy(text);
+							resolve(tippy);;
+
+						} else if (node.data('nodeType') === 'reaction') {
+							_this.get('store').findRecord(
+								'reaction', node.data('reactionId'), { reload: true }).then( function (response) {
+									response.getImage().then(function(response) {
+										 let text =`
+											 <div>
+												 ${response.data.image}
+											 </div>
+											 <p>${ node.data('name')}</p>
+										 `;
+										 let tippy = _makeTippy(text);
+										 resolve(tippy);
+									});
+								}
+							);
+						}
+					});
 				};
 
 				cy.nodes().on('tap', function(evt) {
 					let node = evt.target;
+					let nodeId = node.data('id')
 					if (node.hasTippy) {
 						let tippy = node.tippy
 						if(tippy.state.visible) {
@@ -125,8 +164,24 @@ export default Mixin.create({
 							tippy.show();
 						}
 					} else {
-						makeTippy(node, 'foo');
-						node.tippy.show();
+						makeTippy(node).then( function (tippy) {
+							tippy.show();
+							// node.tippy.show();
+							if (node.data('nodeType') === 'molecule') {
+								var viewACS = new ChemDoodle.ViewerCanvas(nodeId, 200, 200);
+								viewACS.specs.bonds_width_2D = .6;
+								viewACS.specs.bonds_saturationWidthAbs_2D = 2.6;
+								viewACS.specs.bonds_hashSpacing_2D = 2.5;
+								viewACS.specs.atoms_font_size_2D = 10;
+								viewACS.specs.atoms_font_families_2D = ['Helvetica', 'Arial', 'sans-serif'];
+								viewACS.specs.atoms_displayTerminalCarbonLabels_2D = true;
+								// var molfile =
+								var moltarget = ChemDoodle.readMOL(node.data('molFile'));
+								moltarget.scaleToAverageBondLength(14.4);
+								viewACS.loadMolecule(moltarget);
+							}
+						});
+
 					}
 				});
 
