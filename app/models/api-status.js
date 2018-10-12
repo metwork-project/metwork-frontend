0bb08d9bba3b8d2396e80c12b3a7448ed26b39a3
YannBeauxis <pro@yannbeauxis.net>
2018-10-12 16:42:03 +0200

    relaod api-status

--- app/models/api-status.js
+++ app/models/api-status.js
@@ -1,4 +1,5 @@
 import DS from 'ember-data';
+import { later } from '@ember/runloop'
 
 export default DS.Model.extend({
 
@@ -6,4 +7,17 @@ export default DS.Model.extend({
   molecules_count: DS.attr('number'),
   achieved_projects_count: DS.attr('number'),
   running_projects_count: DS.attr('number'),
+
+  didLoad: function(){
+		this.poll();
+	},
+
+	poll: function() {
+		var _this = this;
+		later( function() {
+			_this.reload();
+			_this.poll();
+		}, 3000);
+	},
+
 });
