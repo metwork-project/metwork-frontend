76fe5fda088f053e1ed0d573d2e2f7ed43b6112c
Yann Beauxis <pro@yannbeauxis.net>
2019-04-09 17:36:49 +0200

    Update environment.js

--- config/environment.js
+++ config/environment.js
@@ -3,7 +3,7 @@
 module.exports = function(environment) {
     let ENV = {
     modulePrefix: 'metwork-frontend',
-    version: '0.3.1',
+    version: '0.3.4',
     environment,
     host: 'https://somehost.com',
     rootURL: '/',
