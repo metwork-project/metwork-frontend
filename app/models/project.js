5b3b63a93facd59cf0de5a53f75832b9218fb19a
YannBeauxis <pro@yannbeauxis.net>
2019-05-13 19:31:43 +0200

    nav-link projects public or private

--- app/models/project.js
+++ app/models/project.js
@@ -14,6 +14,7 @@ export default DS.Model.extend({
 			}
 	}),
 	description: DS.attr('string'),
+	user_name: DS.attr('string'),
 	public: DS.attr('boolean'),
 	frag_sample: DS.belongsTo('fragsample'),
 	status_code: DS.attr('number',
