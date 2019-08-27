5205fed31586bd8e719ed74574f6918321b5ccc9
Yann Beauxis <pro@yannbeauxis.net>
2019-08-27 13:59:01 +0200

    news from json

--- app/controllers/index.js
+++ app/controllers/index.js
@@ -3,11 +3,20 @@ import { inject as service } from '@ember/service';
 import CytoscapeMixin from 'metwork-frontend/mixins/cytoscape';
 import ENV from '../config/environment'
 import { computed } from '@ember/object';
+import $ from 'jquery';
 
 export default Controller.extend( CytoscapeMixin, {
   session: service('session'),
   apiStatus: service('api-status'),
   version: ENV.version,
+  allNews: null,
+
+  getAllNews: computed('', function() {
+    let self = this
+    $.getJSON("assets/news.json").then(function(data) {
+      self.set('allNews', data);
+    })
+  }),
 
   statusColor: computed( 'apiStatus.{loading,status.available}', function() {
     if (this.get('apiStatus').loading) {
