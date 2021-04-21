f2af0440fb49cf199f9bcead092ba86fc1b45abb
Yann Beauxis <pro@yannbeauxis.net>
2021-04-21 17:17:04 +0200

    status badge

--- app/components/reaction-index-menu.js
+++ app/components/reaction-index-menu.js
@@ -25,6 +25,7 @@ export default Component.extend({
                     res.push({
                         code: status.code,
                         libelle: status.libelle,
+                        class: status.class,
                         checked: this.get('status').includes(status.code)
                     })
                 }
