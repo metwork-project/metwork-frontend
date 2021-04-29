43b3ba44f4dcff9f8ded222417fde9f63305f047
Yann Beauxis <dev@yannbeauxis.net>
2021-04-29 18:25:17 +0200

    anotation filter

--- app/components/annotations-manager.js
+++ app/components/annotations-manager.js
@@ -1,4 +1,4 @@
-import Component from '@ember/component';
+import ItemsManager from 'metwork-frontend/components/items-manager';
 
 let annotationStatus = {
     INIT: { code: 0, libelle: 'undefined', class: 'secondary' },
@@ -6,13 +6,24 @@ let annotationStatus = {
     VALID: { code: 20, libelle: 'putative', class: 'warning' },
     ACTIVE: { code: 30, libelle: 'validated', class: 'success' },
     ERROR: { code: 90, libelle: 'error', class: 'danger' },
-  }
+}
 
-export default Component.extend({
+export default ItemsManager.extend({
 
     availableStatus: [0, 10, 20, 30],
+    dataLabel: 'annotation',
+    itemIdsLabel: "annotation_ids",
 
     getItemStatus() {
         return annotationStatus
     },
+
+    getFilter() {
+        return {
+            status: this.get("status"),
+            project_id: this.get('project_id'),
+            selected: this.get('selected')
+        }
+    },
+
 });
